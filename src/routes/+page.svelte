297f920b865df6db848013e96068a9b944d5efa7
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2026-03-23 12:49:32 -0400

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
 	import ExampleChart from '$lib/components/ExampleChart.svelte';
-  import type { PageData } from './$types';
+	import type { PageData } from './$types';
 
-  let { data }: { data: PageData } = $props();
+	let { data }: { data: PageData } = $props();
 </script>
 
-<main class="max-w-2xl mx-auto px-6 py-16">
+<main class="mx-auto max-w-2xl px-6 py-16">
+	<header class="mb-16">
+		<h1 class="mb-4 text-4xl font-bold">Article Title</h1>
+		<p class="text-lg text-gray-500">
+			A sentance or two about what this is about. Talk about some other things too.
+		</p>
+	</header>
 
-  <header class="mb-16">
-    <h1 class="text-4xl font-bold mb-4">Article Title</h1>
-    <p class="text-lg text-gray-500">A sentance or two about what this is about. Talk about some other things too.</p>
-  </header>
+	<section class="prose prose-lg mb-16 max-w-none">
+		<p>Example paragraph here.</p>
+	</section>
 
-  <section class="prose prose-lg max-w-none mb-16">
-    <p>Example paragraph here.</p>
-  </section>
+	<div class="mb-16">
+		<ExampleChart data={data.data} />
+	</div>
 
-  <div class="mb-16">
-    <ExampleChart data={data.data} />
-  </div>
+	<section class="prose prose-lg mb-16 max-w-none">
+		<p>Example paragraph here.</p>
+	</section>
 
-  <section class="prose prose-lg max-w-none mb-16">
-    <p>Example paragraph here.</p>
-  </section>
+	<div class="mb-16">
+		<ExampleChart data={data.data} />
+	</div>
 
-  <div class="mb-16">
-    <ExampleChart data={data.data} />
-  </div>
-
-  <section class="prose prose-lg max-w-none mb-16">
-    <p>Closing section</p>
-  </section>
-
+	<section class="prose prose-lg mb-16 max-w-none">
+		<p>Closing section</p>
+	</section>
 </main>
